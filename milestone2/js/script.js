26cf9416986a4ce0050ea8f6f539113d7c79d284
Albaer33 <alisi3396@gmail.com>
2021-11-16 16:24:57 +0100

    milestone 2 aggiustata

--- milestone2/js/script.js
+++ milestone2/js/script.js
@@ -1,4 +1,27 @@
-const boxesContainer = document.querySelector('.container');
-console.log(boxesContainer);
-const newBox = ` <div class="box">box1</div> `;
-boxesContainer.innerHTML = newBox;
+const container = document.querySelector('.container');
+for (let i = 1; i <= 100; i++) {
+    // se multiplo di 5 e 3 inserisco come fizzbuzz
+    if ((i % 3)===0 && (i % 5)===0) {
+        const newElement = document.createElement('div');
+        newElement.innerHTML = 'fizzbuzz';
+        container.append(newElement);
+    }
+    // se multiplo di 3 inserisco come fizz
+    else if ((i % 3)===0) {
+        const newElement = document.createElement('div');
+        newElement.innerHTML = 'fizz';
+        container.append(newElement);
+    }
+    // se multiplo di 5 inserisco come buzz
+    else if ((i % 5)===0) {
+        const newElement = document.createElement('div');
+        newElement.innerHTML = 'buzz';
+        container.append(newElement);
+    }
+    // se multiplo non è multiplo di nessuno dei due inserisco come numero
+    else {
+        const newElement = document.createElement('div');
+        newElement.innerHTML = i;
+        container.append(newElement);
+    }
+}
