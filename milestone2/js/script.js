961cffb668abc132b88fa9746c20044d50f6226b
Albaer33 <alisi3396@gmail.com>
2021-11-16 16:41:44 +0100

    pulizia codice

--- milestone2/js/script.js
+++ milestone2/js/script.js
@@ -1,27 +1,21 @@
 const container = document.querySelector('.container');
 for (let i = 1; i <= 100; i++) {
+    const newElement = document.createElement('div');
     // se multiplo di 5 e 3 inserisco come fizzbuzz
     if ((i % 3)===0 && (i % 5)===0) {
-        const newElement = document.createElement('div');
         newElement.innerHTML = 'fizzbuzz';
-        container.append(newElement);
     }
     // se multiplo di 3 inserisco come fizz
     else if ((i % 3)===0) {
-        const newElement = document.createElement('div');
         newElement.innerHTML = 'fizz';
-        container.append(newElement);
     }
     // se multiplo di 5 inserisco come buzz
     else if ((i % 5)===0) {
-        const newElement = document.createElement('div');
         newElement.innerHTML = 'buzz';
-        container.append(newElement);
     }
     // se multiplo non è multiplo di nessuno dei due inserisco come numero
     else {
-        const newElement = document.createElement('div');
         newElement.innerHTML = i;
-        container.append(newElement);
     }
+    container.append(newElement);
 }
